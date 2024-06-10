34bc639e15025fba49fa04012af2485630282b96
huangyuan.888 <huangyuan.888@bytedance.com>
2024-06-10 16:22:37 +0800

    feat: 切换为远程数据库

--- config.ts
+++ config.ts
@@ -1,15 +1,16 @@
-export const mysqlConfig = {
-  host: '127.0.0.1',
-  port: 3306,
-  username: 'root',
-  password: 'hy123456',
-  database: 'wx_bot',
-};
-
 // export const mysqlConfig = {
-//   host: '123.60.1.214',
+//   host: '127.0.0.1',
 //   port: 3306,
 //   username: 'root',
-//   password: '123456',
+//   password: 'hy123456',
 //   database: 'wx_bot',
 // };
+
+
+export const mysqlConfig = {
+  host: '47.121.142.148',
+  port: 3306,
+  username: 'gaofeng',
+  password: '123456789Gf@',
+  database: 'wx_bot',
+};
